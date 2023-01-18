818846a225d8a2d9eb657b4006971cc22ac84bd7
Nitesh <05niteshraj@gmail.com>
2023-01-18 17:43:22 +0530

    Data updated

--- src/controller/users.controller.js
+++ src/controller/users.controller.js
@@ -8,58 +8,47 @@ import HttpStatus from "../domain/httpstatus.js";
 export const createUser = (req, res) => {
   logger.info(`${req.method} ${req.originalUrl} , Creating User...`);
 
-  database.query(
-    QUERY.USER.CREATE,
-    Object.values(req.body),
-    (error, results) => {
-      logger.info(error);
-      if (error) {
-        res
-          .status(HttpStatus.INTERNAL_SERVER_ERROR.code)
+  database.query(QUERY.USER.CREATE, Object.values(req.body), (error, results) => {
+    if(!results) {
 
-          .send(
-            new Response(
-              HttpStatus.INTERNAL_SERVER_ERROR.code,
-              HttpStatus.INTERNAL_SERVER_ERROR.status,
-              `ISE`,
-              { Error: error }
-            )
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR.code)
+
+            .send(new Response(HttpStatus.INTERNAL_SERVER_ERROR.code, HttpStatus.INTERNAL_SERVER_ERROR.status, `ISError`, {'Error' : error})) 
+   
+    } else {     
+      logger.info(`User created...`);
+      database.query(QUERY.USER.EMAIL, [req.body.email], (error, results) => {
+        if (error) {
+          res
+            .status(HttpStatus.NOT_FOUND.code)
+    
+            .send(
+              new Response(
+                HttpStatus.NOT_FOUND.code,
+                HttpStatus.NOT_FOUND.status,
+                `Not Found`
+              )
+            );
+          return;
+        } else {
+          logger.info(results);
+          logger.info(results[0]);
+          logger.info(typeof results);
+          logger.info(JSON.stringify(results));
+          // results = JSON.Object(results);
+          const token = createToken(results[0]);
+          logger.info(token);
+    
+          res.status(HttpStatus.OK.code).send(
+            new Response(HttpStatus.OK.code, HttpStatus.OK.status, {
+              token: token,
+            })
           );
-        
-      } else {
-        logger.info('Done Creating User...');
-      }
+          return;
+        }
+      });
     }
-  );
+});
 
-  database.query(QUERY.USER.EMAIL, [req.body.email], (error, results) => {
-    if (error) {
-      res
-        .status(HttpStatus.NOT_FOUND.code)
 
-        .send(
-          new Response(
-            HttpStatus.NOT_FOUND.code,
-            HttpStatus.NOT_FOUND.status,
-            `Not Found`
-          )
-        );
-      return;
-    } else {
-      logger.info(results);
-      logger.info(results[0]);
-      logger.info(typeof results);
-      logger.info(JSON.stringify(results));
-      // results = JSON.Object(results);
-      const token = createToken(results[0]);
-      logger.info(token);
-
-      res.status(HttpStatus.OK.code).send(
-        new Response(HttpStatus.OK.code, HttpStatus.OK.status, {
-          token: token,
-        })
-      );
-      return;
-    }
-  });
 };
